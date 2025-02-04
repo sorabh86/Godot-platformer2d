9fa1cd2cdcdb24521ff5389d1818837acacb0f6d
sorabh86 <ssorabh.ssharma@gmail.com>
2025-02-04 17:58:49 +0530

    added enemy

---
+++ enemy.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed = 100  # Movement speed
+@export var move_distance = 100  # How far it moves before turning
+var direction = 1  # 1 = right, -1 = left
+var start_position
+
+func _ready():
+	start_position = position.x  # Store initial position
+
+func _physics_process(delta):
+	velocity.x = direction * speed
+	move_and_slide()
+
+	# Change direction when reaching the movement limit
+	if abs(position.x - start_position) > move_distance:
+		direction *= -1  # Reverse direction
+		scale.x *= -1  # Flip sprite horizontally
